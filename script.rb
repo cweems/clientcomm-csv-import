f6b090c2af52001eb465882c22ba5c3053ba6e26
Paras Sanghavi <paras@codeforamerica.org>
2017-08-18 12:51:58 -0700

    skip existing clients

--- script.rb
+++ script.rb
@@ -1,21 +1,22 @@
 require 'csv'
 
-csv_text = "last_name,first_name,phone_number
-PASTE_CONTACTS_HERE"
-
-
-puts 'script running'
 # Set this before running
-email = 'test@example.com'
+email = nil
+csv_text = 'last_name,first_name,phone_number
+PASTE_CONTACTS_HERE'
+
 user = User.find_by!(email: email)
 
 csv = CSV.parse(csv_text, headers: true)
 csv.each do |row|
   entry = row.to_hash
-  Client.create!(
-    phone_number: entry['phone_number'],
-    first_name: entry['first_name'],
-    last_name: entry['last_name'],
-    user: user
-  )
+
+  unless Client.where(phone_number: PhoneNumberParser.normalize(entry['phone_number']), user: user).present?
+    Client.create!(
+        phone_number: entry['phone_number'],
+        first_name: entry['first_name'],
+        last_name: entry['last_name'],
+        user: user
+    )
+  end
 end
